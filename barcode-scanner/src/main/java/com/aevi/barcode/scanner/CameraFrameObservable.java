0b752dc8a512e47c1a7afdaa57c90c18f66f2121
xdevl <xdevl@users.noreply.github.com>
2019-05-23 18:01:15 +0100

    Bug fix about NullPointerException occuring when the Camera cannot be opened

--- barcode-scanner/src/main/java/com/aevi/barcode/scanner/CameraFrameObservable.java
+++ barcode-scanner/src/main/java/com/aevi/barcode/scanner/CameraFrameObservable.java
@@ -1,19 +1,26 @@
 package com.aevi.barcode.scanner;
 
 import android.graphics.SurfaceTexture;
-import android.hardware.camera2.*;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
 import android.util.Size;
 import android.view.Surface;
-import io.reactivex.*;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
 
 import java.util.Arrays;
 
-public class CameraFrameObservable implements ObservableOnSubscribe<Image> {
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Scheduler;
+import io.reactivex.functions.Function;
+
+public class CameraFrameObservable {
 
     public interface ImageReaderFactory {
 
@@ -22,55 +29,27 @@ public class CameraFrameObservable implements ObservableOnSubscribe<Image> {
 
     public static Observable<Image> create(CameraManager cameraManager, Observable<CameraDevice> cameraObservable,
                                            Observable<Surface> surfaceTextureObservable, ImageReaderFactory imageReaderFactory, Scheduler scheduler) {
-        return Observable.create(new CameraFrameObservable(cameraManager, cameraObservable, surfaceTextureObservable, imageReaderFactory, scheduler));
+        return cameraObservable.zipWith(surfaceTextureObservable, (cameraDevice, surface) -> Tuple.of(cameraDevice, surface))
+                .concatMap(params -> {
+                    CameraDevice camera = params.t1;
+                    Surface surface = params.t2;
+                    Size imageReaderSize = findOptimalSize(cameraManager, camera, 1080, 720, 0d);
+                    final ImageReader imageReader = imageReaderFactory.create(imageReaderSize.getWidth(), imageReaderSize.getHeight(), 3);
+                    return CaptureSessionObservable.create(camera, Arrays.asList(surface, imageReader.getSurface()))
+                            .concatMap((Function<CameraCaptureSession, ObservableSource<ImageReader>>) cameraCaptureSession -> {
+                                        CaptureRequest.Builder captureRequestBuilder =
+                                                cameraCaptureSession.getDevice().createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                                        captureRequestBuilder.addTarget(surface);
+                                        captureRequestBuilder.addTarget(imageReader.getSurface());
+                                        cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+                                        return Observable.just(imageReader);
+                                    }
+                            );
+                }).flatMap((Function<ImageReader, ObservableSource<Image>>) imageReader -> ImageObservable.create(imageReader, 500, scheduler));
     }
 
-    private final CameraManager cameraManager;
-    private final Observable<CameraDevice> cameraObservable;
-    private final Observable<Surface> surfaceTextureObservable;
-    private final ImageReaderFactory imageReaderFactory;
-    private final Scheduler scheduler;
-    private volatile ObservableEmitter<? super Image> observableEmitter;
-    private volatile Disposable disposable;
-
-    public CameraFrameObservable(CameraManager cameraManager, Observable<CameraDevice> cameraObservable, Observable<Surface> surfaceTextureObservable,
-                                 ImageReaderFactory imageReaderFactory, Scheduler scheduler) {
-        this.cameraManager = cameraManager;
-        this.cameraObservable = cameraObservable;
-        this.surfaceTextureObservable = surfaceTextureObservable;
-        this.imageReaderFactory = imageReaderFactory;
-        this.scheduler = scheduler;
-    }
-
-    @Override
-    public void subscribe(final ObservableEmitter<Image> emitter) {
-        observableEmitter = emitter;
-        if (!observableEmitter.isDisposed()) {
-            observableEmitter.setCancellable(() -> disposable.dispose());
-
-            disposable = cameraObservable.zipWith(surfaceTextureObservable, (cameraDevice, surface) -> Tuple.of(cameraDevice, surface))
-                    .concatMap(params -> {
-                        CameraDevice camera = params.t1;
-                        Surface surface = params.t2;
-                        Size imageReaderSize = findOptimalSize(camera, 1080, 720, 0d);
-                        final ImageReader imageReader = imageReaderFactory.create(imageReaderSize.getWidth(), imageReaderSize.getHeight(), 3);
-                        return CaptureSessionObservable.create(camera, Arrays.asList(surface, imageReader.getSurface()))
-                                .concatMap((Function<CameraCaptureSession, ObservableSource<ImageReader>>) cameraCaptureSession -> {
-                                            CaptureRequest.Builder captureRequestBuilder =
-                                                    cameraCaptureSession.getDevice().createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                                            captureRequestBuilder.addTarget(surface);
-                                            captureRequestBuilder.addTarget(imageReader.getSurface());
-                                            cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null);
-                                            return Observable.just(imageReader);
-                                        }
-                                );
-                    }).flatMap((Function<ImageReader, ObservableSource<Image>>) imageReader
-                            -> ImageObservable.create(imageReader, 500, scheduler))
-                    .subscribe(image -> observableEmitter.onNext(image), throwable -> observableEmitter.onError(throwable));
-        }
-    }
-
-    private Size findOptimalSize(CameraDevice cameraDevice, int width, int height, double ratioDelta) throws CameraAccessException {
+    private static Size findOptimalSize(CameraManager cameraManager, CameraDevice cameraDevice, int width, int height, double ratioDelta)
+            throws CameraAccessException {
         CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraDevice.getId());
         StreamConfigurationMap configuration = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
         Size[] sizes = configuration.getOutputSizes(SurfaceTexture.class);
--- barcode-scanner/src/main/java/com/aevi/barcode/scanner/SurfaceObservable.java
+++ barcode-scanner/src/main/java/com/aevi/barcode/scanner/SurfaceObservable.java
@@ -2,57 +2,34 @@ package com.aevi.barcode.scanner;
 
 import android.graphics.SurfaceTexture;
 import android.view.Surface;
+
 import com.aevi.barcode.scanner.SurfaceTextureObservable.Callback;
 import com.aevi.barcode.scanner.Tuple.Tuple2;
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.disposables.Disposable;
 
-public class SurfaceObservable implements ObservableOnSubscribe<Surface> {
+import io.reactivex.Observable;
+
+public class SurfaceObservable {
 
     public interface SurfaceFactory {
+
         Surface create(SurfaceTexture surfaceTexture);
     }
 
     public static Observable<Surface> create(
             Observable<Tuple2<Callback, SurfaceTexture>> surfaceTextureObservable, SurfaceFactory surfaceFactory) {
-        return Observable.create(new SurfaceObservable(surfaceTextureObservable, surfaceFactory));
+
+        return Observable.using(
+                () -> new Surface[]{null},
+                (ref) -> surfaceTextureObservable
+                        .filter(tuple -> Callback.AVAILABLE.equals(tuple.t1))
+                        .map(tuple -> (ref[0] = surfaceFactory.create(tuple.t2))),
+                (ref) -> releaseIfNotNull(ref[0])
+        );
     }
 
-    private final Observable<Tuple2<Callback, SurfaceTexture>> surfaceTextureObservable;
-    private final SurfaceFactory surfaceFactory;
-    private volatile ObservableEmitter<? super Surface> observableEmitter;
-    private volatile Disposable disposable;
-    private volatile Surface surface;
-
-    public SurfaceObservable(
-            Observable<Tuple2<Callback, SurfaceTexture>> surfaceTextureObservable, SurfaceFactory surfaceFactory) {
-        this.surfaceTextureObservable = surfaceTextureObservable;
-        this.surfaceFactory = surfaceFactory;
-    }
-
-    @Override
-    public void subscribe(ObservableEmitter<Surface> emitter) {
-        observableEmitter = emitter;
-        if (!observableEmitter.isDisposed()) {
-            observableEmitter.setCancellable(() -> {
-                if (surface != null) {
-                    surface.release();
-                }
-                disposable.dispose();
-            });
-
-            disposable = surfaceTextureObservable
-                    .filter(tuple -> Callback.AVAILABLE.equals(tuple.t1))
-                    .map(tuple -> tuple.t2)
-                    .subscribe(surfaceTexture -> observableEmitter.onNext(createSurface(surfaceTexture)),
-                            throwable -> observableEmitter.onError(throwable));
+    private static void releaseIfNotNull(Surface surface) {
+        if (surface != null) {
+            surface.release();
         }
-    }
-
-    private Surface createSurface(SurfaceTexture surfaceTexture) {
-        surface = surfaceFactory.create(surfaceTexture);
-        return surface;
     }
 }
